9b0ea386c06a82bc472c1cf1a39527b6bc6ea4c6
Karan <risingphoenix160@gmail.com>
2024-06-16 14:33:16 +0530

    fix: change api base url

--- client/src/app/axios.js
+++ client/src/app/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = "/api/v1";
+const BASE_URL = `${import.meta.env.VITE_API_BASE_URL}/api/v1`;
 
 export default axios.create({
   baseURL: BASE_URL,
--- client/vite.config.js
+++ client/vite.config.js
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ command, mode }) => {
-  const env = loadEnv(mode, process.cwd(), "");
+  // const env = loadEnv(mode, process.cwd(), "");
 
   return {
     plugins: [react()],
@@ -13,12 +13,12 @@ export default defineConfig(({ command, mode }) => {
         "@": path.resolve(__dirname, "./src"),
       },
     },
-    server: {
-      proxy: {
-        "/api": {
-          target: env.VITE_API_BASE_URL,
-        },
-      },
-    },
+    // server: {
+    //   proxy: {
+    //     "/api": {
+    //       target: env.VITE_API_BASE_URL,
+    //     },
+    //   },
+    // },
   };
 });
