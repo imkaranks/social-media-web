610c45e8e5a14035781835734399664281446ed2
Karan <risingphoenix160@gmail.com>
2024-06-16 14:53:33 +0530

    cors

--- server/src/app.js
+++ server/src/app.js
@@ -4,16 +4,30 @@ import cors from "cors";
 import errorHandler from "./middlewares/errorHandler.middleware.js";
 
 const app = express();
+// CORS configuration
+const corsOptions = {
+  origin: process.env.CORS_ORIGIN,
+  credentials: true,
+};
 
 app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
-app.use(
-  cors({
-    origin: process.env.CORS_ORIGIN,
-    credentials: true,
-  })
-);
+app.use(cors(corsOptions));
 app.use(cookieParser());
+
+// Additional CORS headers for OPTIONS requests
+app.options("*", cors(corsOptions));
+
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", process.env.CORS_ORIGIN);
+  res.header(
+    "Access-Control-Allow-Methods",
+    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+  );
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  res.header("Access-Control-Allow-Credentials", "true");
+  next();
+});
 
 /* ######### Routers ######### */
 
